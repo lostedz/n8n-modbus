c22bb3697ba27cff0eb58e3a9f737a876c912db6
Alex Alves <pynjunior@gmail.com>
2025-02-21 20:28:16 +0000

    Add Operation Mode: Read and Write

--- README.md
+++ README.md
@@ -6,29 +6,21 @@
 
 This is a n8n community node that triggers workflows based on Modbus events. It allows you to connect to Modbus devices and react to their data changes in real-time.
 
-## Features
-
-- Connect to Modbus TCP devices
-- Trigger workflows on data changes
-- Supports multiple data types (coil, discrete input, holding register, input register)
-
 ## Installation
 
 Follow the [installation guide](https://docs.n8n.io/integrations/community-nodes/installation/) in the n8n community nodes documentation.
 
-## Configuration
+## Operations
 
-The Modbus Trigger node requires the following configuration:
+- Connect to Modbus TCP devices
+- Trigger workflows on data changes
+- Read and Write (holding register)
+
+## Credentials
 
 - **Host**: The IP address of the Modbus device.
 - **Port**: The port number of the Modbus device (default is 502).
-- **Address**: The address of the data to monitor.
-- **Poll Interval**: The interval at which to poll the Modbus device for changes.
 
 ## Support
 
 If you encounter any issues or have questions, please open an issue on the [GitHub repository](https://github.com/lostedz/n8n-nodes-modbus-trigger).
-
-## License
-
-This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
---
+++ nodes/Modbus/Modbus.node.ts
@@ -0,0 +1,138 @@
+import type {
+	INodeType,
+	INodeTypeDescription,
+	IExecuteFunctions,
+	INodeExecutionData,
+	IDataObject,
+} from 'n8n-workflow';
+import { NodeConnectionType, NodeOperationError } from 'n8n-workflow';
+import { createClient, type ModbusCredential } from './GenericFunctions';
+
+export class Modbus implements INodeType {
+	description: INodeTypeDescription = {
+		displayName: 'MODBUS',
+		name: 'modbus',
+		icon: 'file:modbus.svg',
+		group: ['input'],
+		version: 1,
+		description: 'Read and write to MODBUS devices',
+		eventTriggerDescription: '',
+		defaults: {
+			name: 'MODBUS',
+		},
+		usableAsTool: true,
+		inputs: [NodeConnectionType.Main],
+		outputs: [NodeConnectionType.Main],
+		credentials: [
+			{
+				name: 'modbusApi',
+				required: true,
+			},
+		],
+		properties: [
+			{
+				displayName: 'Operation',
+				name: 'operation',
+				type: 'options',
+				options: [
+					{
+						name: 'Read',
+						value: 'read',
+					},
+					{
+						name: 'Write',
+						value: 'write',
+					},
+				],
+				default: 'read',
+				description: 'The operation to perform in the modbus device',
+			},
+			{
+				displayName: 'Memory Address',
+				name: 'memoryAddress',
+				type: 'number',
+				default: 1,
+				description: 'The memory address (register index) to read from or write to',
+			},
+			{
+				displayName: 'Quantity',
+				displayOptions: {
+					show: {
+						operation: ['read'],
+					},
+				},
+				name: 'quantity',
+				type: 'number',
+				default: 1,
+				description: 'The number of registers to read from',
+			},
+			{
+				displayName: 'Value',
+				displayOptions: {
+					show: {
+						operation: ['write'],
+					},
+				},
+				name: 'value',
+				type: 'number',
+				typeOptions: {
+					maxValue: 32767,
+					minValue: -32768,
+				},
+				default: 1,
+				description: 'The value to write to the memory address',
+			},
+		],
+	};
+
+	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+		let responseData: IDataObject = {};
+		const credentials = await this.getCredentials<ModbusCredential>('modbusApi');
+		const client = await createClient(credentials);
+
+		const memoryAddress = this.getNodeParameter('memoryAddress', 0) as number;
+		const operation = this.getNodeParameter('operation', 0) as string;
+
+		if (operation === 'read') {
+			const quantity = this.getNodeParameter('quantity', 0) as number;
+			await new Promise((resolve) => {
+				client.readHoldingRegisters({ address: memoryAddress, quantity }, (err, data) => {
+					if (err) {
+						throw new NodeOperationError(this.getNode(), 'MODBUS Error: ' + err.message);
+					}
+
+					const returnData: IDataObject = {
+						data: data?.response.data?.map((value) => value.readInt16BE(0)),
+					};
+
+					responseData = returnData;
+					resolve(responseData);
+				});
+			});
+		}
+
+		if (operation === 'write') {
+			const value = this.getNodeParameter('value', 0) as number;
+
+			const buffer = Buffer.alloc(2);
+			buffer.writeInt16BE(value);
+
+			await new Promise((resolve) => {
+				client.writeSingleRegister({ address: memoryAddress, value: buffer }, (err, data) => {
+					if (err) {
+						throw new NodeOperationError(this.getNode(), 'MODBUS Error: ' + err.message);
+					}
+
+					const returnData: IDataObject = {
+						data: data.response,
+					};
+
+					responseData = returnData;
+					resolve(responseData);
+				});
+			});
+		}
+
+		return [this.helpers.returnJsonArray(responseData)];
+	}
+}
